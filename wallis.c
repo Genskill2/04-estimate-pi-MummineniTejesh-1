4297539dacbf63c7ee33ec6734d1eae1f7fff298
MummineniTejesh <84696989+MummineniTejesh@users.noreply.github.com>
2021-06-03 20:09:11 +0530

    solution wallis

--- wallis.c
+++ wallis.c
@@ -23,16 +23,14 @@ int main(void) {
     }
   }
 }
-float wallis_pi(int x)
-{
+float wallis_pi(int x){
 	float a,b,c,pi,i;
 	c=1.0;
-		for(i=1;i<=x;i++)
-		{
+		for(i=1;i<=x;i++){
 			a=4*i*i;
 			b=a-1;
 			c=c*(a/b);
-		}
+			}
 	pi=2*c;
 	return pi;
 }
